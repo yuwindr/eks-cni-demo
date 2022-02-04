b2e220d6cbf879a2bf9d6b3ad36080267da6e424
yuwindr <yuwindr@amazon.com>
2022-02-04 11:44:00 +0800

    modified SG rules that allow to the world

--- network.tf
+++ network.tf
@@ -155,7 +155,7 @@ resource "aws_security_group" "ec2_sg" {
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      cidr_blocks      = ["${var.eks_vpc_main_cidr}"]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []
@@ -166,7 +166,7 @@ resource "aws_security_group" "ec2_sg" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      cidr_blocks      = ["${var.eks_vpc_main_cidr}"]
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []
